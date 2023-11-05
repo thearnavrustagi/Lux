2aadf811adbc551cab074aa37fb85f02f3f65f99
armanghosh10 <149370200+armanghosh10@users.noreply.github.com>
2023-11-05 12:25:40 +0530

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,2 +1,6 @@
-class Dataset (object):
-    pass
+class Dataset(object):
+    def __getitem__(self, idx):
+        raise NotImplementedError("Subclasses of Dataset should implement __getitem__.")
+
+    def __len__(self):
+        raise NotImplementedError("Subclasses of Dataset should implement __len__.")
