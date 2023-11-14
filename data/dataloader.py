ff6fcb737797c2bf1e9b65034d83f4a58b989658
armanghosh10 <149370200+armanghosh10@users.noreply.github.com>
2023-11-15 00:02:10 +0530

    Update dataloader.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,2 +1,6 @@
 class DataLoader (object):
-    pass
+     def __iter__(self):
+        raise NotImplementedError("Subclasses of Dataloader should implement __iter__.")
+
+    def __len__(self):
+        raise NotImplementedError("Subclasses of Dataloader should implement __len__.")
