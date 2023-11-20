aa374552c42326c77092ae14c61b30d2d36834ff
armanghosh10 <149370200+armanghosh10@users.noreply.github.com>
2023-11-20 15:34:17 +0530

    Update dataloader.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,25 +1,28 @@
-def DataLoaderFunc(dataset, batch_size, drop_last, shuffle=False):
-    new_arr = np.ndarray()
-    index = 0
-    for x in ((len(dataset)//batch_size) + 1):
-        for i in batch_size:
-            a = np.ndarray()
-            np.append(a, dataset[index])
-            index += 1
-        np.append(new_arr, a)
-    if drop_last == True and len(new_arr[-1]) < batch_size:
-        new_arr = new_arr[:-1]
-    return new_arr
+import numpy as np 
+import pandas as pd 
 
-class CustomDataLoader:
+def batch_data(dataset, batch_size, drop_last, shuffle=False):
+    batches = []
+    index = 0
+    for i in range(len(dataset)):
+        x_val = np.squeeze(dataset[i:i+batch_size])
+        batches.append(x_val)
+    if drop_last == True and len(batches[-1]) < batch_size:
+        batches = batches[:-1]
+    print(len(batches))
+    batches = np.array(batches)
+    print(batches.shape)
+    return batches
+
+class DataLoader:
     def __init__ (self, dataset, batch_size = 1, shuffle  = False, num_workers = 0, drop_last = False):
         self.dataset = dataset 
         self.batch_size = batch_size
         self.shuffle = shuffle 
         self.num_workers = num_workers
         self.drop_last = drop_last
-        self.dataloader = DataLoaderFunc(dataset=dataset,batch_size=batch_size,shuffle=shuffle,
-        num_workers=num_workers,drop_last=drop_last)
+        self.dataloader = batch_data(dataset=dataset,batch_size=batch_size,shuffle=shuffle,
+                                     drop_last=drop_last)
     
     def __iter__(self):
         return iter(self.dataloader)
@@ -29,3 +32,4 @@ class CustomDataLoader:
             return len(self.dataloader) - 1 if len(self.dataloader) > 0 else 0 
         else: 
             return len(self.dataloader)
+        
